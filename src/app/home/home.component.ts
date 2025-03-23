0806fdf15955863a14344bf47c5d5f5d875e9b05
Martiniano Olivera <martiniano.olivera@sidesys.com>
2025-03-22 22:09:48 -0300

    Se elimina codigo viejo de cuando se guardaba todo en localStorage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -54,96 +54,35 @@ ngOnInit(): void {
 
 }
 
-// onSubmit(): void {
-//   const data = {
-//     movie: 'Contenido de prueba 2',
-//     series: 'Contenido de prueba 2',
-//     food: 'Contenido de prueba 2',
-//     plan: 'Contenido de prueba 2'
-//   };
 
-//   this.firebaseService.addData(data).subscribe(
-//     (res) => {
-//       console.log('Datos subidos exitosamente:', res);
-//     },
-//     (err) => {
-//       console.error('Error al subir los datos:', err);
-//     }
-//   );
-// }
 
 save(){
-
   let data: Activity = {} as Activity;
 
-
-
-  // let title:string = '';
-  // switch (activity) {
-    // case 'película':
-      if (this.movieInput !== '' && this.selectedPlatform !== 'Seleccioná en qué plataforma se encuentra') {
-        // title = 'La película';
-        data.type = 'pelicula'
-        data.nombre = this.movieInput;
-        data.plataforma = this.selectedPlatform;
-        // const movie = `La película <b>${this.movieInput}</b> se puede ver por <b>${this.selectedPlatform}</b>`
-        // const movie = new Activity(this.movieInput,this.selectedPlatform);
-        // this.saveItem('movie',movie);
-        this.movieInput='';
-        this.selectedPlatform = 'Seleccioná en qué plataforma se encuentra';
-        // this.showSuccessMessage(title); 
-      }
-      else{
-        this.showErrorMessage();
-      }
-      // break;
-    // case 'serie':
-      if (this.serieInput !== '' && this.selectedSeriesPlatform !== 'Seleccioná en qué plataforma se encuentra') {
-        // title = 'La serie';
-        data.type = 'serie'
-        data.nombre = this.serieInput;
-        data.plataforma = this.selectedSeriesPlatform;
-        // const serie = new Activity(this.serieInput,this.selectedSeriesPlatform);
-        // this.saveItem('series',serie);
-        this.serieInput='';
-        this.selectedSeriesPlatform = 'Seleccioná en qué plataforma se encuentra';
-        //  this.showSuccessMessage(title);
-      }
-      else{
-        this.showErrorMessage();
-      }
-      // break;
-    // case 'comida':
-      if (this.foodInput !== '') {
-        // title = 'La comida';
-        data.type = 'comida'
-        data.nombre = this.foodInput;
-        // const food = new Activity(this.foodInput);
-        // this.saveItem('food',food);
-        this.foodInput='';
-        //  this.showSuccessMessage(title);
-      }
-      else{
-        this.showErrorMessage();
-      }
-      // break;
-    // case 'plan':
-      if (this.planInput !== '') {
-        // title = 'El plan';
-        data.type = 'plan';
-        data.nombre = this.planInput;
-        // const plan = new Activity(this.planInput);
-        // this.saveItem('plan',plan);
-        this.planInput='';
-        //  this.showSuccessMessage(title);
-      }
-      else{
-        this.showErrorMessage();
-      }
-      // break;
-    // default:
-      // break;
-  // }
+  if (this.movieInput !== '' && this.selectedPlatform !== 'Seleccioná en qué plataforma se encuentra') {
+    data.type = 'pelicula'
+    data.nombre = this.movieInput;
+    data.plataforma = this.selectedPlatform;
+    this.movieInput='';
+    this.selectedPlatform = 'Seleccioná en qué plataforma se encuentra';
+  }
+  if (this.serieInput !== '' && this.selectedSeriesPlatform !== 'Seleccioná en qué plataforma se encuentra') {
+    data.type = 'serie'
+    data.nombre = this.serieInput;
+    data.plataforma = this.selectedSeriesPlatform;
+    this.serieInput='';
+    this.selectedSeriesPlatform = 'Seleccioná en qué plataforma se encuentra';
+  }
+  if (this.foodInput !== '') {
+    data.type = 'comida'
+    data.nombre = this.foodInput;
+    this.foodInput='';
+  }
+  if (this.planInput !== '') {
+    data.type = 'plan';
+    data.nombre = this.planInput;
+    this.planInput='';
+  }
 
   this.firebaseService.addData(data).subscribe({
     next: (res) => {
@@ -155,7 +94,6 @@ save(){
       this.showErrorMessage();
     }
   });
-
 }
 
   toggleDropdown() {
@@ -179,47 +117,6 @@ showSuccessMessage(){
   });
 }
 
-//! Esto podría ser refactorizable tranquilamente. Hay un método "Save" que después llama a este método saveItem.
-//! Para lo unico que sirve saveItem es por el tema del id autoincremental que hay en el modelo Activity. 
-//! Se podría refactorizar para que el id se genere en el método save y no en el saveItem.
-//! Lo ideal sería manejarlo por el lado del backend, pero como no hay backend, se podría hacer en el front.
-//! De momento se va a hacer que funcione así, una vez que funcione bien de esta forma se empezará a trabajar sobre su refactorización.
-//! Paso a paso dijo mostaza.
-//  saveItem(activityName:string, activity:Activity){
-//   console.log(activity);
-//   const storedActivity = window.localStorage.getItem(activityName);
-//   // if (window.localStorage.getItem(activityName) === null){
-//     let name:string = '';
-//   switch (activityName) {
-//     case 'movie':
-//       name = `La película <b>${this.movieInput}</b> tiene el identificador ${activity.id} y se puede ver por <b>${this.selectedPlatform}</b>`
-      
-//       break;
-//     case 'series':
-//       name = `La serie <b>${this.serieInput}</b> tiene el identificador ${activity.id} y se puede ver por <b>${this.selectedSeriesPlatform}</b>`    
-//       break;
-//     case 'food':
-//       name = `La comida es <b>${this.foodInput}</b> y tiene el identificador ${activity.id}`
-//       break;
-//     case 'plan':
-//     name = `El plan es <b>${this.planInput}</b> y tiene el identificador ${activity.id}`
-//       break;
-//     default:
-//       break;
-//   }
-
-//   if (storedActivity === null){
-//     // const value = JSON.stringify((activity.name)) + ', ' + JSON.stringify(activity.information) + ', ' + activity.id;
-//     // window.localStorage.setItem(activityName, value);
-//     window.localStorage.setItem(activityName, name);
-//   }
-//   else{
-//     // const value =  JSON.stringify((activity.name)) + ', ' + JSON.stringify(activity.information);
-//     let finalValue = storedActivity + ', ' + name ;
-//     window.localStorage.setItem(activityName, finalValue);
-//   }
-// }
-
 chooseRandom(){
   const planStored = window.localStorage.getItem('plan');
   const foodStored = window.localStorage.getItem('food');
@@ -241,6 +138,7 @@ chooseRandom(){
     icon: "success"
   });
 }
+
 viewList(){
   this.iMatDialog.open(ListComponent,{
     width: '80%',
@@ -248,4 +146,5 @@ viewList(){
     autoFocus: false,
   });
 }
+
 }
